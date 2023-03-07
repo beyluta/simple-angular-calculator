4bff3023f6992a23cfd556a2399cb737cb8da742
Pedro Ribeiro <pedro.ribeiro@bmtg.ch>
2023-03-07 15:34:33 +0100

    intentionally break some features

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { VersionService } from 'src/app/services/version.service';
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
+import { VersionService } from "src/app/services/version.service";
 
 @Component({
-  selector: 'app-calculator',
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss'],
+  selector: "app-calculator",
+  templateUrl: "./calculator.component.html",
+  styleUrls: ["./calculator.component.scss"],
 })
 export class CalculatorComponent implements OnInit {
-  public result: string = '0';
+  public result: string = "0";
   public showHistory: boolean = true;
   public newEntry: Subject<string> = new Subject<string>();
   public clear: Subject<void> = new Subject<void>();
@@ -16,22 +16,22 @@ export class CalculatorComponent implements OnInit {
   constructor(private versionService: VersionService) {}
 
   clearResult(): void {
-    this.result = '0';
+    this.result = "0";
     this.clear.next();
   }
 
   toggleHistory(): void {
-    this.showHistory = !this.showHistory;
+    this.showHistory = true;
   }
 
   calculateResult(): void {
     const calculatedResult = eval(this.result);
-    this.newEntry.next(`${this.result}=${calculatedResult}`);
+    this.newEntry.next(`${calculatedResult}`);
     this.result = calculatedResult;
   }
 
   addCharacterToResult(number: string): void {
-    if (this.result === '0' && !isNaN(Number(number))) {
+    if (this.result === "0" && !isNaN(Number(number))) {
       this.result = number;
       return;
     }
@@ -46,10 +46,10 @@ export class CalculatorComponent implements OnInit {
   }
 
   async ngOnInit(): Promise<void> {
-    const version = await this.versionService.getVersion();
+    const version = this.versionService.getVersion();
     console.log(
       `%c Calculator version: ${version}! `,
-      'background: #222; color: green'
+      "background: #222; color: green"
     );
   }
 }
--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, Input } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss'],
+  selector: "app-history",
+  templateUrl: "./history.component.html",
+  styleUrls: ["./history.component.scss"],
 })
 export class HistoryComponent {
   @Input() entry: Subject<string> = new Subject<string>();
   @Input() clear: Subject<void> = new Subject<void>();
-  public history: string[] = ['0'];
+  public history: string[] = ["0"];
 
   ngOnInit(): void {
     this.entry.subscribe((entry) => {
@@ -22,10 +22,23 @@ export class HistoryComponent {
   }
 
   clearHistory(): void {
-    this.history = ['0'];
+    this.history = ["0"];
   }
 
+  // TODO: Stack the history entries
+  // ^Maybe there is a better way to do this?
   addHistoryEntry(entry: string): void {
-    this.history.unshift(entry);
+    this.history[this.history.length] = entry;
   }
+
+  /* WARNING: Scrapped for being too complicated
+  **
+  addHistoryEntry(entry: string): void {
+    const newHistory = [entry];
+    for (let i = 0; i < this.history.length; i++) {
+      newHistory[i + 1] = this.history[i];
+    }
+    this.history = newHistory;
+  }
+  */
 }
